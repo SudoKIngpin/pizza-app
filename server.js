b3192e2f472ee6fa5e06965b8743bc9eed5670f3
sudoKIngpin <wd51886@gmail.com>
2024-12-15 16:27:54 +0530

    Add cart page and login/reg page added

--- server.js
+++ server.js
@@ -1,25 +1,59 @@
 const express=require('express')
+const ejs=require('ejs') ///template engine
 
-const app=express()
-const ejs = require('ejs')
-const expressLayout = require('express-ejs-layouts')
-const PORT=process.env.PORT||3300
-const path=require('path')
+const expressLayout=require('express-ejs-layouts')
+//use for avoid repetition and make a layout 
+//dynamic code of diff. pages  wil be injected in layout.ejs file 
 
+const app=express() // express pbject
+
+const path=require('path') 
+
+const PORT=process.env.PORT || 3000 //PORT environment variable
+
+
+
+
+
+
+//Assests 
+
+app.use(express.static('public'));
+
+
+// set template engine 
 app.use(expressLayout)
-app.set('views',path.join(__dirname,'/resources/views'))
-app.set('view engine','ejs')
 
-app.get("/",(req,res)=>{
-    //res.send("Hello from server");
-    res.render('home')
+app.set('views',path.join(__dirname,"/resources/views"))
+app.set("view engine",'ejs')
+
+
+
+
+app.get('/cart',(req,res)=>{    // defining routes 
+    res.render("customers/cart")
 
 })
 
 
-app.listen(PORT,()=>{
-    console.log(`Listening on ${PORT}....`)
+app.get('/',(req,res)=>{    // defining routes 
+    res.render("home")
 })
 
 
+app.get('/login',(req,res)=>{    // defining routes 
+    res.render("auth/login")
+})
+
+app.get('/register',(req,res)=>{    // defining routes 
+    res.render("auth/register")
+})
+
+
+
+
+
+app.listen(PORT,()=>{ 
+    console.log(`Listening on port ${PORT} `)
+})
 
